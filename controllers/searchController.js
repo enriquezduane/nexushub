d739df7e62ea1ef23b3c730e1e400708743bb7a7
Aspectzxcc <josh10nathan@gmail.com>
2024-02-28 18:17:36 +0800

    added search pagination

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -5,6 +5,12 @@ const { populatePosts, highlightSubstring, populateUsers } = require('./helper')
 const renderSearch = (req, res) => {
     try {
         // Render the search page
+        if (req.query.page) {
+            if (req.query.page < 1 || req.query.page > res.totalPages || isNaN(req.query.page)) {
+                return res.status(404).json({ message: 'Page not found' });
+            }
+        }
+        
         res.render('search', { 
             loggedIn: req.isAuthenticated(), 
             title: 'Search Results', 
@@ -12,6 +18,9 @@ const renderSearch = (req, res) => {
             posts: res.posts, 
             users: res.users,
             query: req.query.query, 
+            page: res.page,
+            totalPages: res.totalPages,
+            url: req.originalUrl,
             highlightSubstring, 
             forumRules: res.forumRules, 
             userLoggedIn: req.user
@@ -28,22 +37,37 @@ const searchFilter = async (req, res, next) => {
     try {
         const query = req.query.query;
         const target = req.query.target;
+        const page = parseInt(req.query.page) || 1;
+        const limit = 1;
+
+        let startIndex = (page - 1) * limit;
+        let endIndex = startIndex + limit;
 
         if (query !== "") {
             if (target.toLowerCase() === 'posts') {
-            const posts = await Post.find({ title: { $regex: new RegExp(query, 'i') } });
-            res.posts = await populatePosts(posts);
+                const posts = await Post.find({ title: { $regex: new RegExp(query, 'i') } });
+                const totalPosts = posts.length;
+                const totalPages = Math.ceil(totalPosts / limit);
+                const results = posts.slice(startIndex, endIndex);
+                res.posts = await populatePosts(results);
+                res.totalPages = totalPages;
+                res.page = page;
             } else if (target.toLowerCase() === 'users') {
                 const users = await User.find({ username: { $regex: new RegExp(query, 'i') } });
-                res.users = await populateUsers(users);
+                const totalUsers = users.length;
+                const totalPages = Math.ceil(totalUsers / limit);
+                const results = users.slice(startIndex, endIndex);
+                res.users = await populateUsers(results);
+                res.totalPages = totalPages;
+                res.page = page;
             }
         }
         next();
-    }
-    catch (err) {
+    } catch (err) {
         res.status(500).json({ message: err.message });
     }
 }
+
 
 module.exports = {
     renderSearch,
