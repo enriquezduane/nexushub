eebf5b9257c5ffc4e263c45c9c784a7a6ae6da62
Aspectzxcc <josh10nathan@gmail.com>
2024-03-24 02:02:12 +0800

    fixed post/reply overflows with images and long single strings, removed image toolbar from text editor

--- public/js/quillEditor.js
+++ public/js/quillEditor.js
@@ -56,7 +56,7 @@ function initializeQuill(className) {
                 [{ 'header': '1'}, { 'header': '2' }, 'blockquote', 'code-block'],
                 [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'indent': '-1'}, { 'indent': '+1' }],
                 ['direction', { 'align': [] }],
-                ['link', 'image', 'video', 'clean'],
+                ['link', 'video', 'clean'],
             ],
             handlers: {}
         };
