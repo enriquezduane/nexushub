1ad6c23f32ae0ae01f81de46fb5faf82e1d3413a
Aspectzxcc <josh10nathan@gmail.com>
2024-03-23 18:00:17 +0800

    added npm run seed command for seeding db

--- models/database/seed.js
+++ models/database/seed.js
@@ -9,7 +9,7 @@ const Report = require('../reportModel');
 const dotenv = require('dotenv');
 dotenv.config();
 
-MONGODB_URL = process.env.MONGODB_URL || 'mongodb+srv://joshuatating-windows:nprnJWaALuh9VRjC@nexushub.lj3y5hg.mongodb.net/?retryWrites=true&w=majority&appName=NexusHub';
+MONGODB_URL = process.env.MONGODB_URL;
 
 async function seedDatabase() {
     try {
